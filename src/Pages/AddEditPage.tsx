436ebad55008535116cac3edf5f4e0a057a36738
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-09-27 10:08:57 +0700

    commit

--- src/Pages/AddEditPage.tsx
+++ src/Pages/AddEditPage.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import React, {useState, useEffect} from "react"
-import { useParams, useNavigate } from "react-router-dom"
+import React, {useState, useEffect} from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { API_URL, ERROR_MESSAGE } from "../constants";
 import isEmpty from "lodash/isEmpty";
 
--- src/Pages/ListPage.tsx
+++ src/Pages/ListPage.tsx
@@ -4,10 +4,16 @@ import { API_URL } from "../constants";
 import {useNavigate} from "react-router-dom"
 import axios from "axios";
 import "./style.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
 export default function App() {
-  const [students, setStudents] = useState<any>([])
-  const [showPopup, setShowPopup] = useState<boolean>(false)
+  const [students, setStudents] = useState<any>([]);
+  const [showPopup, setShowPopup] = useState<boolean>(false);
+  const [sortByName, setSortByName] = useState<boolean>(false);
+  const [sortByDate, setSortByDate] = useState<boolean>(false);
+  const [sortByMonth, setSortByMonth] = useState<boolean>(false);
+  const [isAsc, setIsAsc] = useState<boolean>(true);
+
   const navigate = useNavigate()
 
 
@@ -20,33 +26,102 @@ export default function App() {
     navigate(`/create`)
   }
 
-  const renderStudents = () => {
-    
-  }
+  const renderStudents = async () => {
 
+    try {
+        const resp = await axios.get('https://random-data-api.com/api/v2/users?size=10');
+        setStudents(resp.data);
+        console.log(resp.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    useEffect(() => {
+      renderStudents();
+
+    }, []);
   const onSortByName = () => {
-
+    if (isAsc) {
+      setSortByName(true);
+      setSortByDate(false);
+      setSortByMonth(false);
+      setIsAsc(false);
+    }
+    if (!isAsc) {
+      setSortByName(true);
+      setSortByDate(false);
+      setSortByMonth(false);
+      setIsAsc(true);
+    }
   }
 
   const onSortByDate = () => {
-
+    if (isAsc) {
+      setSortByDate(true);
+      setSortByName(false);
+      setSortByMonth(false);
+      setIsAsc(false)
+    }
+    if (!isAsc) {
+      setSortByDate(true);
+      setSortByName(false);
+      setSortByMonth(false);
+      setIsAsc(true)
+    }
   }
 
   const onSortByMonth = () => {
-
+    if (isAsc) {
+      setSortByMonth(true);
+      setSortByName(false);
+      setSortByDate(false);
+      setIsAsc(false)
+    }
+    if (!isAsc) {
+      setSortByMonth(true);
+      setSortByName(false);
+      setSortByDate(false);
+      setIsAsc(true)
+    }
   }
 
   return (
     <div className="App">
       <div className="student-table-wrapper">
-        <table className="student-table">
-          <tr>
-            <th className="student-col">Name</th>
-            <th className="student-col">Date of birth</th>
-            <th></th>
-          </tr>
-          {renderStudents()}
-        </table>
+        <Table striped bordered hover className="student-table">
+          <thead>
+            
+            <tr>
+              <th>First Name</th>
+              <th>Date of birth</th>
+            </tr>
+          </thead>
+          <tbody>
+          {!sortByName && !sortByDate && !sortByMonth ? students.map((ele:any) => (
+              <tr key={ele.uid}>
+                <td>{ele.first_name}</td>
+                <td>{ele.date_of_birth}</td>
+            </tr>
+            )): sortByName ? students.sort((a:any,b:any) => isAsc ? ((a.first_name > b.first_name) ? 1 : -1) : ((a.first_name < b.first_name) ? 1 : -1)).map((ele:any) => (
+              <tr key={ele.uid}>
+                <td>{ele.first_name}</td>
+                <td>{ele.date_of_birth}</td>
+            </tr>
+            )) : sortByDate ? students.sort((a:any,b:any) => isAsc ? ((new Date(a.date_of_birth) > new Date(b.date_of_birth)) ? 1 : -1) : 
+            ((new Date(a.date_of_birth) < new Date(b.date_of_birth)) ? 1 : -1)).map((ele:any) => (
+              <tr key={ele.uid}>
+                <td>{ele.first_name}</td>
+                <td>{ele.date_of_birth}</td>
+            </tr>
+            )) : sortByMonth ? students.sort((a:any,b:any) => isAsc ? ((new Date(a.date_of_birth).getMonth() > new Date(b.date_of_birth).getMonth()) ? 1 : -1) : 
+            ((new Date(a.date_of_birth).getMonth() < new Date(b.date_of_birth).getMonth()) ? 1 : -1)).map((ele:any) => (
+              <tr key={ele.uid}>
+                <td>{ele.first_name}</td>
+                <td>{ele.date_of_birth}</td>
+            </tr>
+            )) : ''}
+          </tbody>
+        </Table>
       </div>
       <div className="button-wrapper">
         <button onClick={onSortByName} className="button">Sort By Name</button>
